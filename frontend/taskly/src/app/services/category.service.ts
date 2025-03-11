22ab9f7392317a80de703005548ada5ac1a0205b
tabish29 <tabish13295@gmail.com>
2025-03-11 15:08:26 +0100

    aggiunte le chiamate per la creazione/modifica/eliminazione delle categorie

--- frontend/taskly/src/app/services/category.service.ts
+++ frontend/taskly/src/app/services/category.service.ts
@@ -15,4 +15,16 @@ private apiUrl = `http://localhost:8080/api/categories`;
   getCategories(): Observable<Category[]> {
     return this.http.get<Category[]>(this.apiUrl);
   }
+
+  createCategory(category: Category): Observable<Category> {
+    return this.http.post<Category>(this.apiUrl, { name: category.name });
+  }
+
+  updateCategory(category: Category): Observable<Category> {
+    return this.http.put<Category>(`${this.apiUrl}/${category.id}`, category);
+  }
+
+  deleteCategory(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
